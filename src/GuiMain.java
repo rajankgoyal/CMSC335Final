3fe092cb142d80dcc28d3c94be554252a2c86de2
Rajan Goyal <goyal.rajan@yahoo.com>
2021-12-14 00:16:19 -0500

    Intersections are working. They are updating their correspondence Traffic Lights.

--- src/GuiMain.java
+++ src/GuiMain.java
@@ -1,18 +1,20 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class GuiMain extends JFrame{
+public class GuiMain extends JFrame {
+
 
     static JLabel timeLabel = new JLabel(" 14:32");
+
 
     JButton startButton = new JButton("Start");
     JButton stopButton = new JButton("Stop");
     JButton continueButton = new JButton("Continue");
     JButton pauseButton = new JButton("Pause");
 
-    JLabel intersection1Status = new JLabel("Green");
-    JLabel intersection2Status = new JLabel("Yellow");
-    JLabel intersection3Status = new JLabel("Red");
+    public static JLabel intersection1Status = new JLabel("Green");
+    static JLabel intersection2Status = new JLabel("Yellow");
+    static JLabel intersection3Status = new JLabel("Red");
 
     JLabel car1Speed = new JLabel("100 mph");
     JLabel car1Location = new JLabel("500, 0");
@@ -20,6 +22,15 @@ public class GuiMain extends JFrame{
     JLabel car2Location = new JLabel("1500, 0");
     JLabel car3Speed = new JLabel("100 mph");
     JLabel car3Location = new JLabel("2500, 0");
+
+    Thread worker = new WorkerThread();
+    public static TrafficLight trafficLight1 = new TrafficLight(TrafficLightColor.GREEN, "tl1");
+    //public static Thread trafficLightThread1 = new Thread(trafficLight1);
+    public static TrafficLight trafficLight2 = new TrafficLight(TrafficLightColor.YELLOW, "tl2");
+    //public static Thread trafficLightThread2 = new Thread(trafficLight2);
+    public static TrafficLight trafficLight3 = new TrafficLight(TrafficLightColor.RED, "tl3");
+    //public static Thread trafficLightThread3 = new Thread(trafficLight3);
+
     public GuiMain() {
 
         // Creates a new JFrame.
@@ -46,7 +57,7 @@ public class GuiMain extends JFrame{
         JLabel intersectionDis1 = new JLabel("1000 Meters");
         intersectionPanel1.add(intersection1);
         intersectionPanel1.add(intersectionDis1);
-        intersectionPanel1.add(Box.createRigidArea(new Dimension(0,10)));
+        intersectionPanel1.add(Box.createRigidArea(new Dimension(0, 10)));
         intersectionPanel1.add(intersection1Status);
 
         // Intersection 2 panel
@@ -56,7 +67,7 @@ public class GuiMain extends JFrame{
         JLabel intersectionDis2 = new JLabel("2000 Meters");
         intersectionPanel2.add(intersection2);
         intersectionPanel2.add(intersectionDis2);
-        intersectionPanel2.add(Box.createRigidArea(new Dimension(0,10)));
+        intersectionPanel2.add(Box.createRigidArea(new Dimension(0, 10)));
         intersectionPanel2.add(intersection2Status);
 
         // Intersection 3 panel
@@ -66,7 +77,7 @@ public class GuiMain extends JFrame{
         JLabel intersectionDis3 = new JLabel("3000 Meters");
         intersectionPanel3.add(intersection3);
         intersectionPanel3.add(intersectionDis3);
-        intersectionPanel3.add(Box.createRigidArea(new Dimension(0,10)));
+        intersectionPanel3.add(Box.createRigidArea(new Dimension(0, 10)));
         intersectionPanel3.add(intersection3Status);
 
 
@@ -77,13 +88,13 @@ public class GuiMain extends JFrame{
         JLabel carSpeedDis1 = new JLabel("Speed");
         JLabel carLocationDis1 = new JLabel("Location");
         carPanel1.add(car1);
-        carPanel1.add(Box.createRigidArea(new Dimension(0,10)));
+        carPanel1.add(Box.createRigidArea(new Dimension(0, 10)));
         carPanel1.add(carSpeedDis1);
         carPanel1.add(car1Speed);
-        carPanel1.add(Box.createRigidArea(new Dimension(0,10)));
+        carPanel1.add(Box.createRigidArea(new Dimension(0, 10)));
         carPanel1.add(carLocationDis1);
         carPanel1.add(car1Location);
-        carPanel1.add(Box.createRigidArea(new Dimension(0,10)));
+        carPanel1.add(Box.createRigidArea(new Dimension(0, 10)));
         // Cars 2 panel
         JPanel carPanel2 = new JPanel();
         carPanel2.setLayout(new BoxLayout(carPanel2, BoxLayout.PAGE_AXIS));
@@ -91,13 +102,13 @@ public class GuiMain extends JFrame{
         JLabel carSpeedDis2 = new JLabel("Speed");
         JLabel carLocationDis2 = new JLabel("Location");
         carPanel2.add(car2);
-        carPanel2.add(Box.createRigidArea(new Dimension(0,10)));
+        carPanel2.add(Box.createRigidArea(new Dimension(0, 10)));
         carPanel2.add(carSpeedDis2);
         carPanel2.add(car2Speed);
-        carPanel2.add(Box.createRigidArea(new Dimension(0,10)));
+        carPanel2.add(Box.createRigidArea(new Dimension(0, 10)));
         carPanel2.add(carLocationDis2);
         carPanel2.add(car2Location);
-        carPanel2.add(Box.createRigidArea(new Dimension(0,10)));
+        carPanel2.add(Box.createRigidArea(new Dimension(0, 10)));
         // Cars 3 panel
         JPanel carPanel3 = new JPanel();
         carPanel3.setLayout(new BoxLayout(carPanel3, BoxLayout.PAGE_AXIS));
@@ -105,43 +116,43 @@ public class GuiMain extends JFrame{
         JLabel carSpeedDis3 = new JLabel("Speed");
         JLabel carLocationDis3 = new JLabel("Location");
         carPanel3.add(car3);
-        carPanel3.add(Box.createRigidArea(new Dimension(0,10)));
+        carPanel3.add(Box.createRigidArea(new Dimension(0, 10)));
         carPanel3.add(carSpeedDis3);
         carPanel3.add(car3Speed);
-        carPanel3.add(Box.createRigidArea(new Dimension(0,10)));
+        carPanel3.add(Box.createRigidArea(new Dimension(0, 10)));
         carPanel3.add(carLocationDis3);
         carPanel3.add(car3Location);
-        carPanel3.add(Box.createRigidArea(new Dimension(0,10)));
+        carPanel3.add(Box.createRigidArea(new Dimension(0, 10)));
         // Buttons for control
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.LINE_AXIS));
         buttonPanel.add(startButton);
-        buttonPanel.add(Box.createRigidArea(new Dimension(5,0)));
+        buttonPanel.add(Box.createRigidArea(new Dimension(5, 0)));
         buttonPanel.add(stopButton);
-        buttonPanel.add(Box.createRigidArea(new Dimension(5,0)));
+        buttonPanel.add(Box.createRigidArea(new Dimension(5, 0)));
         buttonPanel.add(continueButton);
-        buttonPanel.add(Box.createRigidArea(new Dimension(5,0)));
+        buttonPanel.add(Box.createRigidArea(new Dimension(5, 0)));
         buttonPanel.add(pauseButton);
         // Added intersections
         JPanel intersectionPanel = new JPanel();
         intersectionPanel.setLayout(new BoxLayout(intersectionPanel, BoxLayout.LINE_AXIS));
         intersectionPanel.add(intersectionPanel1);
-        intersectionPanel.add(Box.createRigidArea(new Dimension(50,100)));
+        intersectionPanel.add(Box.createRigidArea(new Dimension(50, 100)));
         intersectionPanel.add(intersectionPanel2);
-        intersectionPanel.add(Box.createRigidArea(new Dimension(50,100)));
+        intersectionPanel.add(Box.createRigidArea(new Dimension(50, 100)));
         intersectionPanel.add(intersectionPanel3);
         // Added cars
         JPanel carPanel = new JPanel();
         carPanel.setLayout(new BoxLayout(carPanel, BoxLayout.LINE_AXIS));
         carPanel.add(carPanel1);
-        carPanel.add(Box.createRigidArea(new Dimension(25,100)));
+        carPanel.add(Box.createRigidArea(new Dimension(25, 100)));
         carPanel.add(carPanel2);
-        carPanel.add(Box.createRigidArea(new Dimension(25,100)));
+        carPanel.add(Box.createRigidArea(new Dimension(25, 100)));
         carPanel.add(carPanel3);
         // Panel to include Time and intersections
         JPanel upperPanel = new JPanel();
         upperPanel.setLayout(new BoxLayout(upperPanel, BoxLayout.PAGE_AXIS));
-        upperPanel.add(Box.createRigidArea(new Dimension(25,25)));
+        upperPanel.add(Box.createRigidArea(new Dimension(25, 25)));
         // Added the time panel to main panel
         upperPanel.add(timePanel);
         upperPanel.add(intersectionPanel);
@@ -156,29 +167,65 @@ public class GuiMain extends JFrame{
         frame.add(mainPanel);
         frame.setVisible(true);
     }
-    private void buttons(){
+
+    public void runThread() {
+
+    }
+
+    private void buttons() {
         startButton.addActionListener(e -> {
-            JOptionPane.showMessageDialog(null, "Only enter positive numerical value");
+//            JOptionPane.showMessageDialog(null, "Only enter positive numerical value");
+
+            trafficLight1.thread.start();
+            trafficLight2.thread.start();
+            trafficLight3.thread.start();
+            if (!worker.isAlive()) {
+                worker.start();
+
+            } else {
+                worker.resume();
+            }
 
         });
 
         stopButton.addActionListener(e -> {
-            JOptionPane.showMessageDialog(null, "Only enter positive numerical value");
+            worker.suspend();
         });
 
 
         continueButton.addActionListener(e -> {
-            JOptionPane.showMessageDialog(null, "Only enter positive numerical value");
+            worker.resume();
         });
 
 
         pauseButton.addActionListener(e -> {
-            JOptionPane.showMessageDialog(null, "Only enter positive numerical value");
+            worker.suspend();
+
         });
     }
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(GuiMain::new);
         Thread timer = new Thread(new Timer());
         timer.start();
+
+    }
+}
+
+class WorkerThread extends Thread {
+    @Override
+    public void run() {
+        while (true) {
+            GuiMain.intersection1Status.setText(GuiMain.trafficLight1.getColor().toString());
+            GuiMain.trafficLight1.waitForChange();
+
+            GuiMain.intersection2Status.setText(GuiMain.trafficLight2.getColor().toString());
+            GuiMain.trafficLight2.waitForChange();
+
+            GuiMain.intersection3Status.setText(GuiMain.trafficLight3.getColor().toString());
+            GuiMain.trafficLight3.waitForChange();
+        }
+
+        //GuiMain.tl.cancel();
     }
 }
---
+++ src/TrafficLight.java
@@ -0,0 +1,88 @@
+// Try This 12-1
+
+// A simulation of a traffic light that uses 
+// an enumeration to describe the light's color. 
+ 
+// An enumeration of the colors of a traffic light. 
+enum TrafficLightColor {  
+  RED, GREEN, YELLOW 
+}
+
+// A computerized traffic light. 
+public class TrafficLight implements Runnable {
+  private TrafficLightColor tlc; // holds the current traffic light color 
+  private boolean stop = false; // set to true to stop the simulation 
+  private boolean changed = false; // true when the light has changed
+  Thread thread;
+  TrafficLight(TrafficLightColor init, String name) {
+    tlc = init;
+    thread= new Thread( this, name);
+  } 
+ 
+  TrafficLight() {
+    tlc = TrafficLightColor.RED; 
+  } 
+ 
+  // Start up the light. 
+  public void run() {
+    while(!stop) {
+      System.out.println(tlc);
+      try {
+        switch(tlc) {
+          case GREEN: 
+            Thread.sleep(1100); // green for 10 seconds
+            break; 
+          case YELLOW: 
+            Thread.sleep(500);  // yellow for 2 seconds
+            break; 
+          case RED: 
+            Thread.sleep(1000); // red for 12 seconds
+            break; 
+        } 
+      } catch(InterruptedException exc) { 
+        System.out.println(exc); 
+      } 
+      changeColor(); 
+    }  
+  } 
+ 
+  // Change color. 
+  synchronized void changeColor() { 
+    switch(tlc) { 
+      case RED: 
+        tlc = TrafficLightColor.GREEN; 
+        break; 
+      case YELLOW: 
+        tlc = TrafficLightColor.RED; 
+        break; 
+      case GREEN: 
+       tlc = TrafficLightColor.YELLOW; 
+    } 
+ 
+    changed = true;
+    notify(); // signal that the light has changed 
+  } 
+ 
+  // Wait until a light change occurs. 
+  synchronized void waitForChange() { 
+    try { 
+      while(!changed) 
+        wait(); // wait for light to change 
+      changed = false;
+    } catch(InterruptedException exc) { 
+      System.out.println(exc); 
+    } 
+  } 
+ 
+  // Return current color. 
+  synchronized TrafficLightColor getColor() { 
+    return tlc; 
+  } 
+ 
+  // Stop the traffic light. 
+  synchronized void cancel() { 
+    stop = true; 
+  }
+
+
+}
